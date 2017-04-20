323ef1a82a32e5e76602ba57e28f933c151e7dba
hhabib <hanaqudsi@gmail.com>
2017-04-20 15:06:20 -0400

    seperating js code from html

---
+++ app/code/static/js/categorize_attr.js
@@ -0,0 +1,72 @@
+var attributes = [];
+
+$(document).ready(function() {
+
+    //Retrieve stored attributes from backend.
+    $.getJSON($SCRIPT_ROOT + '/_python2array', function(data){
+        attributes = data.result;
+
+        // Print attributes
+        var html = '<form><tbody>';
+  
+        for(var attr in attributes) {
+            html += '<tr>\r\n';
+            html += '<td>' + attributes[attr] + '</td>\r\n<td><select class=\"form-control\" id=\"' + attributes[attr] + '\" style=\"width: 300px\">\r\n<option value=\"identifying\">Identifying</option>\r\n<option value=\"sensitive\">Sensitive</option>\r\n<option value=\"age\">Age</option>\r\n<option value=\"zip\">Zip</option>\r\n</select></td>\r\n';
+            html += '</tr>\r\n<br>';
+        }
+        html += '</tbody></form>';
+    
+        $('#attrList').html(html);
+
+     });
+    
+});
+
+// Pass user input to backend
+function submit() { 
+    var attrCategorization = {};
+    var identifyingAttr = [];
+    var sensitiveAttr = [];
+    var insensitiveAttr = [];
+    var age = [];
+    var zip = [];
+    var klevel = document.getElementById("klevel").value;
+    for (var attr in attributes) {
+        var selection = document.getElementById(attributes[attr]).value;
+        
+        if (selection == 'identifying') {
+            identifyingAttr.push(attributes[attr]);
+        } 
+
+        if (selection == 'sensitive') {
+            sensitiveAttr.push(attributes[attr]);
+        }
+
+        if (selection == 'insensitive') {
+            insensitiveAttr.push(attributes[attr]);
+        }
+
+        if (selection == 'age') {
+            age.push(attributes[attr]);
+        }
+
+        if (selection == 'zip') {
+            zip.push(attributes[attr]);
+        }
+    }
+
+    attrCategorization["identifying"] = identifyingAttr;
+    attrCategorization["sensitive"] = sensitiveAttr;
+    attrCategorization["insensitive"] = insensitiveAttr;
+    attrCategorization["age"] = age;
+    attrCategorization["zip"] = zip;
+    attrCategorization["k"] = klevel;
+
+    $.getJSON($SCRIPT_ROOT + '/_categorization2python', {
+        attrCategorization: JSON.stringify(attrCategorization)
+    }, function(data){
+        console.log(data.result)
+    });
+
+    window.location = "./anonymize.html";   
+}
---
+++ app/code/static/js/file_upload.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+    // The event listener for the file upload
+    document.getElementById('csvFileUpload').addEventListener('change', upload, false);
+
+    // Method that checks that the browser supports the HTML5 File API
+    function browserSupportFileUpload() {
+        var isCompatible = false;
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            isCompatible = true;
+        }
+        return isCompatible;
+    }
+
+    // Method that reads and processes the selected file
+    function upload(evt) {
+        if (!browserSupportFileUpload()) {
+            alert('The File APIs are not fully supported in this browser!');
+        } else {
+                var data = null;
+                var file = evt.target.files[0];
+
+                // Ajax Post the file to the backend
+                var formData = new FormData();
+                formData.append('file', file);
+                $.ajax({
+                       url : '/postcsv',
+                       type : 'POST',
+                       data : formData,
+                       processData: false,  // tell jQuery not to process the data
+                       contentType: false,  // tell jQuery not to set contentType
+                       success : function(data) {
+                           console.log(data);
+                       }
+                });
+
+                var attributes = null;
+                var reader = new FileReader();
+                reader.readAsText(file);
+                reader.onload = function(event) {
+                    var csvData = event.target.result;
+                    data = $.csv.toArrays(csvData);
+                    attributes = data[0];
+                    
+                    // Store attributes in backend.
+                    $.getJSON($SCRIPT_ROOT + '/_array2python', {
+                        attributes: JSON.stringify(attributes)
+                    }, function(data){
+                        console.log(data.result)
+                    });
+
+                    // Print Data
+                    var html = '<thead>\r\n<tr>';
+                    for(var cell in data[0]) {
+                        html += '<th>' + data[0][cell] + '</th>\r\n'
+                    }
+                    html += '</tr>\r\n</thead>\r\n<tbody>'
+                    
+                    for(var row in data) {
+                        if(row > 0 && row < 5) {
+                            html += '<tr>\r\n';
+                            for(var item in data[row]) {
+                                html += '<td>' + data[row][item] + '</td>\r\n';
+                            }
+                            html += '</tr>\r\n' ;
+                        }
+                    }
+                    html += '</tbody>'
+
+                    $('#contents').html(html);
+
+                    // Add previous & next buttons
+                    $('#navigation').html('<ul class=\"pager\"><li class=\"previous\"><a href=\"./\">Back</a></li><li class=\"next\"><a href=\"./anonymize.html\">Next</a></li></ul>');
+                };
+                reader.onerror = function() {
+                    console.log('Unable to read ' + file.fileName);
+                };
+            }
+    }
+}); 
